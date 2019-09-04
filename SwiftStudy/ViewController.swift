4773f531bfc5ad7be5566fb1aa4365ced517fb3f
guoliting <guoliting@didichuxing.com>
2019-09-04 15:32:14 +0800

    初始化工程

---
+++ SwiftStudy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by guoliting on 2019/8/27.
+//  Copyright © 2019 guoliting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let myString = "Hello, World!"
+        print(myString)
+    }
+
+
+}
+
